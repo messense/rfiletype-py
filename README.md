9d0257fbaf365ba1fb6dc75b9e04e803e2476300
messense <messense@icloud.com>
2021-04-08 17:04:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# rfiletype-py
+
+![CI](https://github.com/messense/rfiletype-py/workflows/CI/badge.svg)
+[![PyPI](https://img.shields.io/pypi/v/rfiletype.svg)](https://pypi.org/project/rfiletype)
+
+[infer](https://github.com/bojand/infer) Python binding, infer file and MIME
+type by checking the magic number signature.
+
+## Installation
+
+```bash
+pip install rfiletype
+```
+
+## License
+
+This work is released under the MIT license. A copy of the license is provided in the [LICENSE](./LICENSE) file.
---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+/// Gets the type of a file from a byte stream.
+///
+/// Returns MIME as string.
+#[pyfunction]
+fn from_buffer(buf: &[u8]) -> Option<&'static str> {
+    infer::get(buf).map(|x| x.mime_type())
+}
+
+/// Gets the type of a file from a filepath.
+///
+/// Does not look at file name or extension, just the contents.
+/// Returns MIME as string
+#[pyfunction]
+fn from_file(path: &str) -> PyResult<Option<&'static str>> {
+    infer::get_from_path(path)
+        .map(|x| x.map(|t| t.mime_type()))
+        .map_err(|e| pyo3::exceptions::PyOSError::new_err(e.to_string()))
+}
+
+/// tree-magic determines the MIME type a given file or byte stream
+#[pymodule]
+fn tree_magic(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    m.add_function(wrap_pyfunction!(from_buffer, m)?)?;
+    m.add_function(wrap_pyfunction!(from_file, m)?)?;
+
+    Ok(())
+}
