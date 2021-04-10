fffcf8b95237e799ed74e9fd9c1ca181106015b6
messense <messense@icloud.com>
2021-04-10 21:49:20 +0800

    Add Type class

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,33 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+/// File type
+#[pyclass(module = "rfiletype")]
+#[derive(FromPyObject)]
+struct Type {
+    /// file mime type
+    #[pyo3(get)]
+    mime_type: String,
+    /// file extension
+    #[pyo3(get)]
+    extension: String,
+}
+
+impl From<infer::Type> for Type {
+    fn from(ty: infer::Type) -> Self {
+        Type {
+            mime_type: ty.mime_type().to_string(),
+            extension: ty.extension().to_string(),
+        }
+    }
+}
+
 /// Gets the type of a file from a byte stream.
 ///
 /// Returns MIME as string.
 #[pyfunction]
-fn from_buffer(py: Python, buf: &[u8]) -> Option<&'static str> {
-    py.allow_threads(|| infer::get(buf).map(|x| x.mime_type()))
+fn from_buffer(py: Python, buf: &[u8]) -> Option<Type> {
+    py.allow_threads(|| infer::get(buf).map(|x| x.into()))
 }
 
 /// Gets the type of a file from a filepath.
@@ -14,8 +35,8 @@ fn from_buffer(py: Python, buf: &[u8]) -> Option<&'static str> {
 /// Does not look at file name or extension, just the contents.
 /// Returns MIME as string
 #[pyfunction]
-fn from_file(py: Python, path: &str) -> PyResult<Option<&'static str>> {
-    let res = py.allow_threads(|| infer::get_from_path(path).map(|x| x.map(|t| t.mime_type())));
+fn from_file(py: Python, path: &str) -> PyResult<Option<Type>> {
+    let res = py.allow_threads(|| infer::get_from_path(path).map(|x| x.map(|t| t.into())));
     res.map_err(|e| pyo3::exceptions::PyOSError::new_err(e.to_string()))
 }
 
@@ -23,6 +44,7 @@ fn from_file(py: Python, path: &str) -> PyResult<Option<&'static str>> {
 #[pymodule]
 fn rfiletype(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    m.add_class::<Type>()?;
     m.add_function(wrap_pyfunction!(from_buffer, m)?)?;
     m.add_function(wrap_pyfunction!(from_file, m)?)?;
 
